d2c9cf9a5aa9c985bfd18bb69e1c0d5b69a00cea
Anurag-regmi <www.surajdhimal1234@gmail.com>
2025-01-30 17:55:43 +0545

    Updated routing logic in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,38 @@
 <?php
 
+use App\Models\Company;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $companies = Company::all();
+    // return $companies;
+    return view('welcome', compact('companies'));
+});
+
+Route::get("/company-list", function(){
+    $companies = Company::all();
+    return view("company_list", compact('companies'));
 });
 
 Route::get('/about',function(){
     return view('about');
+});
+
+Route::post('/post-company', function(Request $request){
+    $company = new Company();
+
+    $company->name = $request->name;
+    $company->address = $request->address;
+    $company->phone = $request->phone;
+    $company->email = $request->email;
+    $file = $request->logo;
+    if ($file) {
+        $fileName = time().".".$file->getClientOriginalExtension();
+        $file->move('images', $fileName);
+        $company->logo = "images/$fileName";
+    }
+    $company->save();
+
+    return redirect()->back();
 });
