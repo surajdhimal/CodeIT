fca482ba7447d583333b942776d9f068322dac23
Anurag-regmi <www.surajdhimal1234@gmail.com>
2025-01-30 17:55:51 +0545

    Added company list view

---
+++ resources/views/company_list.blade.php
@@ -0,0 +1,28 @@
+<section>
+    <div class="container py-10">
+        <h1 class="text-2xl font-medium text-center mb-5">Company Lists</h1>
+{{ $companies}}
+        <table class="w-full text-center">
+            <thead>
+                <tr>
+                    <th class="border p-2">Name</th>
+                    <th class="border p-2">SN</th>
+                    <th class="border p-2">Email</th>
+                    <th class="border p-2">Phone</th>
+                    <th class="border p-2">Logo</th>
+                    <th class="border p-2">Address</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td class="border p-2">1</td>
+                    <td class="border p-2">Code IT</td>
+                    <td class="border p-2">codeit@gmail.com</td>
+                    <td class="border p-2">9836473829</td>
+                    <td class="border p-2"><img src="" alt=""></td>
+                    <td class="border p-2">Swayambhu, Kathmandu, Nepal</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</section>
