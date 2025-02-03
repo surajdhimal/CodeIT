3d7f001cf8c227956538af347ea04fbc7d94ad50
Anurag-regmi <www.surajdhimal1234@gmail.com>
2025-02-03 14:01:24 +0545

    Updated Course.php model to include relationship with Admission

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Course extends Model
 {
-    //
+        public function admissions(): HasMany
+    {
+        return $this->hasMany(Admission::class);
+    }
 }
