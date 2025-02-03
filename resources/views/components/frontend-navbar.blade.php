cb3d32e35108bde06b04edb070f2aec6682d54b3
Anurag-regmi <www.surajdhimal1234@gmail.com>
2025-02-03 14:01:38 +0545

    Updated frontend-navbar.blade.php to reflect Admission data

--- resources/views/components/frontend-navbar.blade.php
+++ resources/views/components/frontend-navbar.blade.php
@@ -52,7 +52,7 @@
           <a href="/course" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Course</a>
         </li>
         <li>
-          <a href="#" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+          <a href="{{ route('admission.index') }}" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Admissions</a>
         </li>
       </ul>
     </div>
